759b7f6292e9fc862b2ef816e11814b6982dcd46
Yoryo Fuentes <jorge.iv.fu@gmail.com>
2019-09-17 21:18:10 -0300

    Se Agrega toolbar

--- app/src/main/java/com/example/klassi_fe/MainActivity.java
+++ app/src/main/java/com/example/klassi_fe/MainActivity.java
@@ -8,21 +8,16 @@ import android.support.v7.widget.ToolbarWidgetWrapper;
 public class MainActivity extends AppCompatActivity {
 
 
-    Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        toolbar = (Toolbar) findViewById(R.id.idtoolbar);
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().hide();
 
 
         setContentView(R.layout.activity_main);
 
 
 
+
     }
+
 }
