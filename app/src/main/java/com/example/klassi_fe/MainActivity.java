c58c7203c6baea01e6b500ccc683ec43d19e08e3
Lautaro Rasulo <lautarorasulo@gmail.com>
2019-10-02 21:29:18 -0300

    Pantalla de registración Comenté los metodos de post Validar campos

--- app/src/main/java/com/example/klassi_fe/MainActivity.java
+++ app/src/main/java/com/example/klassi_fe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.klassi_fe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,10 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(MainActivity.this , Register.class);
+        startActivity(intent);
 
 
 
--- app/src/main/java/com/example/klassi_fe/Register.java
+++ app/src/main/java/com/example/klassi_fe/Register.java
@@ -1,7 +1,9 @@
 package com.example.klassi_fe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,25 +11,41 @@ public class Register extends AppCompatActivity {
 
 
     EditText txt_name, txt_secndname, txt_email, txt_dni;
+    Button btn_reg;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
-        Button btn_reg;
+
 
         txt_name = findViewById(R.id.txt_name);
         txt_secndname = findViewById(R.id.txt_scndname);
         txt_email = findViewById(R.id.txt_dni);
         txt_dni = findViewById(R.id.txt_dni);
 
-        btn_reg = findViewById(R.id.btn_reg);
+        btn_reg = (Button) findViewById(R.id.btn_reg);
+
+        invocarListeners();
 
     }
 
+    public void invocarListeners(){
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                validate();
+                //POST REGISTRACION
+                /*  intent = new Intent(Register.this ,Login.class);
+                        startActivity(intent);*/
+            }
+        });
+    }
 
-    public void validar(){
+
+    public void validate(){
         if(txt_name.getText().toString().isEmpty()){
             txt_name.setError(getString(R.string.str_error));
         }
@@ -42,3 +60,6 @@ public class Register extends AppCompatActivity {
         }
     }
 }
+
+
+//CREAR METODO POST
